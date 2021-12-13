f49bddeed857119d103f994d1a5448660ac35bae
Andrea-Pinales <95321339+Andrea-Pinales@users.noreply.github.com>
2021-12-13 12:14:36 -0800

    Updated icons images

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//function to last update
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -22,6 +23,7 @@ function formatDate(timestamp) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+//function to show weather details
 function showDetails(response) {
   console.log(response);
   let temperature = document.querySelector("h1");
@@ -30,7 +32,7 @@ function showDetails(response) {
   let wind = document.querySelector("#wind");
   let date = document.querySelector("#current-date");
 
-  temperature.innerHTML = Math.round(response.data.main.temp);
+  temperature.innerHTML = `${Math.round(response.data.main.temp)}°`;
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
   wind.innerHTML = Math.round(response.data.wind.speed);
